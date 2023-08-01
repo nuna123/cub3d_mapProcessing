5094e1a25541d397344524a1a785a20a66661594
Naomi Roth <nroth@c2r6s5.42prague.com>
2023-08-01 13:36:18 +0200

    fixed, kinda norminetted just need to separate files and fix main func

--- mapProcessing.c
+++ mapProcessing.c
@@ -1,113 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapProcessing.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nroth <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 13:08:32 by nroth             #+#    #+#             */
+/*   Updated: 2023/08/01 13:08:34 by nroth            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mapProcessing.h"
 #include "Libft/libft.h"
 
 #include <stdio.h>
 #include <fcntl.h>
 
-void	mapInfo_free(mapInfo_t *mapInfo)
+void	map_info_free(t_mapInfo *map_info)
 {
-	if (!mapInfo)
-		return;
-	if (mapInfo->ceiling_rgb)
-		free(mapInfo->ceiling_rgb);
-	if (mapInfo->floor_rgb)
-		free(mapInfo->floor_rgb);
+	int	i;
 
-
-	if(mapInfo->texture_paths)
+	if (!map_info)
+		return ;
+	if (map_info->ceiling_rgb)
+		free(map_info->ceiling_rgb);
+	if (map_info->floor_rgb)
+		free(map_info->floor_rgb);
+	if (map_info->texture_paths)
 	{
-		if (mapInfo->texture_paths[NO])
-			free(mapInfo->texture_paths[NO]);
-		if (mapInfo->texture_paths[SO])
-			free(mapInfo->texture_paths[SO]);
-		if (mapInfo->texture_paths[WE])
-			free(mapInfo->texture_paths[WE]);
-		if (mapInfo->texture_paths[EA])
-			free(mapInfo->texture_paths[EA]);
-		free (mapInfo->texture_paths);
+		i = -1;
+		while (++i < 4)
+			if (map_info->texture_paths[i])
+				free(map_info->texture_paths[i]);
+		free (map_info->texture_paths);
 	}
-/* 	if (mapInfo->EA_texture_path)
-		free(mapInfo->EA_texture_path);
-	if (mapInfo->WE_texture_path)
-		free(mapInfo->WE_texture_path);
-	if (mapInfo->NO_texture_path)
-		free(mapInfo->NO_texture_path);
-	if (mapInfo->SO_texture_path)
-		free(mapInfo->SO_texture_path); */
-
-	free (mapInfo);
+	free (map_info);
 }
 
-void	error (mapInfo_t *mapInfo, char *error_msg)
+int	error(t_mapInfo *map_info, char *error_msg)
 {
-	mapInfo_free(mapInfo);
+	map_info_free(map_info);
 	printf ("Error\n");
 	printf("%s\n", error_msg);
-	exit (1);
+	return (ERR);
 }
 
-rgb_t *rgb_init(void)
+t_rgb	*rgb_init(void)
 {
-	rgb_t	*rgb;
-	rgb = malloc (sizeof(rgb_t));
+	t_rgb	*rgb;
+
+	rgb = malloc (sizeof(t_rgb));
 	if (!rgb)
 		return (NULL);
 	rgb->red = -1;
 	rgb->green = -1;
 	rgb->blue = -1;
-
 	return (rgb);
 }
 
-mapInfo_t *mapInfo_init(void)
+t_mapInfo	*map_info_init(void)
 {
-	mapInfo_t	*mapInfo;
+	t_mapInfo	*map_info;
 	int			i;
 
-	mapInfo = malloc (sizeof (mapInfo_t));
-	if (!mapInfo)
+	map_info = malloc (sizeof (t_mapInfo));
+	if (!map_info)
 		error (NULL, "Memory Allocation Failed!");
-
-	mapInfo->ceiling_rgb = NULL;
-	mapInfo->floor_rgb = NULL;
-
-	mapInfo->texture_paths = malloc (sizeof (char *) * 5);
+	map_info->ceiling_rgb = NULL;
+	map_info->floor_rgb = NULL;
+	map_info->texture_paths = malloc (sizeof (char *) * 5);
 	i = -1;
 	while (++i < 5)
-		mapInfo->texture_paths[i] = NULL;
-
-
-/* 	mapInfo->NO_texture_path = NULL;
-	mapInfo->SO_texture_path = NULL;
-	mapInfo->WE_texture_path = NULL;
-	mapInfo->EA_texture_path = NULL; */
-
-	return (mapInfo);
+		map_info->texture_paths[i] = NULL;
+	return (map_info);
 }
 
-
-void	textureline_fill(mapInfo_t	*mapInfo, char **mapline_split)
+void	textureline_fill(t_mapInfo	*map_info, char **mapline_split)
 {
 	char	*compass_str;
 	char	**compass_arr;
 	int		i;
 
 	compass_str = ft_strdup("NO|SO|WE|EA");
-	compass_arr = ft_split(compass_str,'|');
+	compass_arr = ft_split(compass_str, '|');
 	i = -1;
-
 	while (++i < 4)
 	{
 		if (ft_strncmp(compass_arr[i], mapline_split[0], 3) == 0)
 		{
-			if (!mapInfo->texture_paths[i])
-				mapInfo->texture_paths[i] = ft_strtrim(mapline_split[1], "\n");
+			if (!map_info->texture_paths[i])
+				map_info->texture_paths[i] = ft_strtrim(mapline_split[1], "\n");
 			else
 			{
 				free (compass_str);
 				ft_arrfree((void **) compass_arr);
 				ft_arrfree((void **) mapline_split);
-				error(mapInfo, "multiple map texture entries!");
+				error(map_info, "multiple map texture entries!");
 			}
 		}
 	}
@@ -115,116 +103,62 @@ void	textureline_fill(mapInfo_t	*mapInfo, char **mapline_split)
 	ft_arrfree((void **) compass_arr);
 }
 
-void	rgb_fill(mapInfo_t	*mapInfo, char **mapline_split)
+int	rgb_fill(t_mapInfo	*map_info, char **mapline_split)
 {
-	printf("RAGB FILL\n");
-	rgb_t	*rgb;
+	t_rgb	*rgb;
 	char	**rgb_arr;
-
-	if ((mapline_split[0][0] == 'F' && !mapInfo->floor_rgb))
-		mapInfo->floor_rgb = rgb_init();
-	else if(mapline_split[0][0] == 'C' && !mapInfo->ceiling_rgb)
-		mapInfo->ceiling_rgb = rgb_init();
-	else
-		error ((ft_arrfree((void **) mapline_split), mapInfo),
-			"Duplicate F/C lines!");
-	rgb = mapInfo->ceiling_rgb;
-	if (mapline_split[0][0] == 'F')
-		rgb = mapInfo->floor_rgb;
-
-	rgb_arr = ft_split(mapline_split[1], ',');
-
-	if (ft_arrlen((void **)rgb_arr) != 3)
-	{
-		ft_arrfree((void **) mapline_split);
-		ft_arrfree((void **) rgb_arr);
-		free (rgb);
-		error (mapInfo, "Invalid F/C line!");
-	}
-	rgb->red = ft_atoi(rgb_arr[0]);
-	rgb->green = ft_atoi(rgb_arr[1]);
-	rgb->blue= ft_atoi(rgb_arr[2]);
-
-	if (rgb->red < 0 || rgb->red > 255 ||
-		rgb->green < 0 || rgb->green > 255 ||
-		rgb->blue < 0 || rgb->blue > 255)
-	{
-		ft_arrfree((void **) mapline_split);
-		ft_arrfree((void **) rgb_arr);
-		free (rgb);
-		error (mapInfo, "Invalid F/C line!");
-	}
-	ft_arrfree((void **) rgb_arr);
-}
-/*
-void	rgb_fill(mapInfo_t	*mapInfo, char **mapline_split)
-{
-	rgb_t	*rgb;
-	char	**rgb_arr;
-
-	if ((mapline_split[0][0] == 'F' && mapInfo->floor_rgb)
-	|| (mapline_split[0][0] == 'C' && mapInfo->ceiling_rgb))
-		error ((ft_arrfree((void **) mapline_split), mapInfo),
-			"Duplicate F/C lines!");
 
 	rgb = rgb_init();
 	rgb_arr = ft_split(mapline_split[1], ',');
-
 	if (ft_arrlen((void **)rgb_arr) != 3)
-	{
-		ft_arrfree((void **) mapline_split);
-		ft_arrfree((void **) rgb_arr);
-		free (rgb);
-		error (mapInfo, "Invalid F/C line!");
-	}
+		return (ft_arrfree((void **) mapline_split),
+			ft_arrfree((void **) rgb_arr),
+			error (map_info, "Invalid F/C line!"));
 	rgb->red = ft_atoi(rgb_arr[0]);
 	rgb->green = ft_atoi(rgb_arr[1]);
-	rgb->blue= ft_atoi(rgb_arr[2]);
-
-	if (rgb->red < 0 || rgb->red > 255 ||
-		rgb->green < 0 || rgb->green > 255 ||
-		rgb->blue < 0 || rgb->blue > 255)
-	{
-		ft_arrfree((void **) mapline_split);
-		ft_arrfree((void **) rgb_arr);
-		free (rgb);
-		error (mapInfo, "Invalid F/C line!");
-	}
-	if (mapline_split[0][0] == 'F')
-		mapInfo->floor_rgb = rgb;
+	rgb->blue = ft_atoi(rgb_arr[2]);
+	if (rgb->red < 0 || rgb->red > 255 || rgb->green < 0
+		|| rgb->green > 255 || rgb->blue < 0 || rgb->blue > 255)
+		return (ft_arrfree((void **) mapline_split), free (rgb),
+			ft_arrfree((void **) rgb_arr),
+			error (map_info, "Invalid F/C line!"));
+	if ((mapline_split[0][0] == 'F' && !map_info->floor_rgb))
+		map_info->floor_rgb = rgb;
+	else if (mapline_split[0][0] == 'C' && !map_info->ceiling_rgb)
+		map_info->ceiling_rgb = rgb;
 	else
-		mapInfo->ceiling_rgb = rgb;
+		return (free(rgb), error ((ft_arrfree((void **) mapline_split),
+					map_info), "Duplicate F/C lines!"));
+	return (ft_arrfree((void **) rgb_arr), OK);
 }
 
- */
-void get_info(mapInfo_t	*mapInfo, char *map_line)
+int	get_info(t_mapInfo	*map_info, char *map_line)
 {
-	printf("GETINFO\n");
 	char	**mapline_split;
 
 	mapline_split = ft_split(map_line, ' ');
 	if (!mapline_split)
-		error (mapInfo, "Error with spitting mapline!");
+		return (error (map_info, "Error with splitting mapline!"));
 	if (ft_arrlen((void **) mapline_split) != 2)
-		error ((ft_arrfree((void **) mapline_split), mapInfo),
-			"Invalid mapline!");
-
+		return (error ((ft_arrfree((void **) mapline_split), map_info),
+				"Invalid mapline!1"));
 	if (ft_strlen(mapline_split[0]) == 2
-		&&	!ft_strchr(mapline_split[0], '|')
-		&&	ft_strnstr("NO|SO|WE|EA", mapline_split[0], 11))
-		textureline_fill(mapInfo, mapline_split);
+		&& !ft_strchr(mapline_split[0], '|')
+		&& ft_strnstr("NO|SO|WE|EA", mapline_split[0], 11))
+		textureline_fill(map_info, mapline_split);
 	else if (ft_strlen(mapline_split[0]) == 1
 		&& (mapline_split[0][0] == 'F' || mapline_split[0][0] == 'C'))
-		rgb_fill(mapInfo, mapline_split);
+	{
+		if (rgb_fill(map_info, mapline_split) == ERR)
+			return (ERR);
+	}
 	else
-		error ((ft_arrfree((void **) mapline_split), mapInfo),
-		"Invalid mapline!");
-
-	ft_arrfree((void **) mapline_split);
+		return (error ((ft_arrfree((void **) mapline_split), map_info),
+				"Invalid mapline!2"));
+	return (ft_arrfree((void **) mapline_split), OK);
 }
 
-
-int	mapInfo_fill(mapInfo_t	*mapInfo, int map_fd)
+int	map_info_fill(t_mapInfo	*map_info, int map_fd)
 {
 	char	*map_line;
 
@@ -233,24 +167,28 @@ int	mapInfo_fill(mapInfo_t	*mapInfo, int map_fd)
 	{
 		if (ft_strncmp(map_line, "\n", 2))
 		{
-			if (!mapInfo->texture_paths[0] || !mapInfo->texture_paths[1]
-				|| !mapInfo->texture_paths[2] || !mapInfo->texture_paths[3]
-				|| !mapInfo->ceiling_rgb|| !mapInfo->floor_rgb)
-				get_info(mapInfo, map_line);
+			if (!map_info->texture_paths[0] || !map_info->texture_paths[1]
+				||!map_info->texture_paths[2] || !map_info->texture_paths[3]
+				|| !map_info->ceiling_rgb || !map_info->floor_rgb)
+			{
+				if (get_info(map_info, map_line) == ERR)
+				{
+					while (map_line)
+						map_line = (free(map_line), get_next_line(map_fd));
+					return (free (map_line), ERR);
+				}
+			}
 		}
-		free(map_line);
-		map_line = get_next_line(map_fd);
+		map_line = (free(map_line), get_next_line(map_fd));
 	}
-	return 0;
+	return (OK);
 }
 
-
-
-int main (int argc, char **argv)
+int	main (int argc, char **argv)
 {
 	char		*map_path = "./map.cub";
 	int			map_fd;
-	mapInfo_t	*mapInfo;
+	t_mapInfo	*map_info;
 
 	if (argc > 1)
 		map_path = argv[1];
@@ -259,25 +197,26 @@ int main (int argc, char **argv)
 	//Check map extension
 	if (ft_strncmp(&(map_path[ft_strlen(map_path) - ft_strlen(".cub")])
 			, ".cub", ft_strlen(".cub")) != 0)
-		error(NULL, "Map is not of type .cub!");
+		return (error(NULL, "Map is not of type .cub!"));
 
 	//check that file exists
 	map_fd = open(map_path, O_RDONLY);
 	if (map_fd == -1)
-		error(NULL, "Map path is invalid!");
+		return (error(NULL, "Map path is invalid!"));
 
-	//init mapInfo, fill
-	mapInfo = mapInfo_init();
-	if (!mapInfo)
-		error((close(map_fd), NULL), "Map initialization failed!");
-	mapInfo_fill(mapInfo, map_fd);
+	//init map_info, fill
+	map_info = map_info_init();
+	if (!map_info)
+		return (printf("Map initialization failed!\n"), ERR);
+
+	if (map_info_fill(map_info, map_fd) == ERR)
+		return (close (map_fd), printf("Map initialization failed!\n"), ERR);
+
+
 	printf ("MAP: \n");
-	printf ("rgb: ceiling(%i,%i,%i) floor(%i,%i,%i)\n", mapInfo->ceiling_rgb->red, mapInfo->ceiling_rgb->green, mapInfo->ceiling_rgb->blue , mapInfo->floor_rgb->red, mapInfo->floor_rgb->green, mapInfo->floor_rgb->blue );
-
-	printf("textures: \n\t NO: %s \n\tSO: %s  \n\tWE: %s  \n\tEA: %s\n", mapInfo->texture_paths[0], mapInfo->texture_paths[1], mapInfo->texture_paths[2], mapInfo->texture_paths[3]);
-
-	mapInfo_free(mapInfo);
-
+	printf ("rgb: ceiling(%i,%i,%i) floor(%i,%i,%i)\n", map_info->ceiling_rgb->red, map_info->ceiling_rgb->green, map_info->ceiling_rgb->blue , map_info->floor_rgb->red, map_info->floor_rgb->green, map_info->floor_rgb->blue );
+	printf("textures: \n\t NO: %s \n\tSO: %s  \n\tWE: %s  \n\tEA: %s\n", map_info->texture_paths[0], map_info->texture_paths[1], map_info->texture_paths[2], map_info->texture_paths[3]);
+	map_info_free(map_info);
 
 	close (map_fd);
 	return 0;
--- mapProcessing.h
+++ mapProcessing.h
@@ -1,33 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapProcessing.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nroth <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 13:08:23 by nroth             #+#    #+#             */
+/*   Updated: 2023/08/01 13:08:27 by nroth            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MAPPROCESSING_H
 # define MAPPROCESSING_H
 
+# define OK 0
+# define ERR 1
 
 # define NO 0
 # define SO 1
 # define WE	2
 # define EA	3
 
-
-typedef struct rgb_s
+typedef struct s_rgb
 {
-	int red;
-	int green;
-	int blue;
-} rgb_t;
+	int	red;
+	int	green;
+	int	blue;
+}	t_rgb;
 
-typedef struct mapInfo_s
+typedef struct s_mapInfo
 {
-	rgb_t	*ceiling_rgb;
-	rgb_t	*floor_rgb;
+	t_rgb	*ceiling_rgb;
+	t_rgb	*floor_rgb;
 
 	char	**texture_paths;
-
-/* 	char	*NO_texture_path;
-	char	*SO_texture_path;
-	char	*WE_texture_path;
-	char	*EA_texture_path; */
-	
-} mapInfo_t;
-
+}	t_mapInfo;
 
 #endif
